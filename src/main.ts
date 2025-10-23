3f77be445d0d506473b6ca7c33e8e73c1988bfe3
AsoYasinMohammed <asoyasin2003@gmail.com>
2025-10-24 01:50:01 +0300

    Importing Axios To main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 import './assets/styles/main.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:4000'
+
+
 
 const pinia = createPinia()
 const app = createApp(App)
