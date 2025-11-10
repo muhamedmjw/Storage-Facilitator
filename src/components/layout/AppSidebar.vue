4fca944f3ceec6ee19dcb1a7f96bd005eec50884
Muhammed <mhammd.best12@gmail.com>
2025-11-10 14:59:12 +0300

    Remove storage and customer reporting tab

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -87,42 +87,6 @@
             <span>Customers</span>
           </router-link>
         </li>
-
-        <li>
-          <router-link
-            to="/reports"
-            class="nav-item"
-          >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <line
-                x1="12"
-                y1="20"
-                x2="12"
-                y2="10"
-              />
-              <line
-                x1="18"
-                y1="20"
-                x2="18"
-                y2="4"
-              />
-              <line
-                x1="6"
-                y1="20"
-                x2="6"
-                y2="16"
-              />
-            </svg>
-            <span>Reports</span>
-          </router-link>
-        </li>
       </ul>
     </nav>
   </aside>
